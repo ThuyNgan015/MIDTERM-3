0f742ecebfac50e4a163a31431f694ddebd30c5a
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-06-08 01:03:14 +0700

    use the Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Search from './components/users/Search';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Search from "./components/users/Search";
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users Data</h1>
-        <Search />
-      </div>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <h1>GitHub Users Data</h1>
+          <Routes>
+            <Route path="/" element={<Search />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 };
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='navbar bg-success'>
+    <nav className="navbar bg-success">
       <h1>
-        <i className='fab fa-github' /> GitHub Finder
+        <i className="fab fa-github" /> GitHub Finder
       </h1>
       <ul>
         <li>
-          <a href='#'>Home</a>
-          <a href='#'>About</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
         </li>
       </ul>
     </nav>
