a1a76460971e91383d631674419c2ae91760f9e3
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-06-08 00:24:16 +0700

    Create Search Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+import Search from './components/users/Search';
 
-function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://api.github.com/users');
-        setUsers(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
-      <div className='container'>
+      <div className="container">
         <h1>GitHub Users Data</h1>
-        <Users users={users} />
+        <Search />
       </div>
     </div>
   );
-}
+};
 
 export default App;
